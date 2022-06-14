70b397433526584c01fd8bf2c51bb7155018b260
kungfu-kenny <oleksandrshevchenko889@gmail.com>
2022-06-15 02:47:10 +0300

    added the file to work on huffman file

---
+++ code_huffman.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+
+
+class CodeHuffman:
+    """
+    class which is dedicated to work with Huffman encoding/decoding
+    """
+    def __init__(self, message:str) -> None:
+        self.message = message
+
+    def produce_encoding_basic(self) -> str:
+        """
+        
+        """
+        pass
+
+
+if __name__ == '__main__':
+    tst = input()
+    a = CodeHuffman(tst)
