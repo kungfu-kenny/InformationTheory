54ab51afa60b7506496ab5c2c0e1955951f68cce
kungfu-kenny <oleksandrshevchenko889@gmail.com>
2022-06-18 02:28:39 +0300

    added the full circle of the developing archive with a mistake

--- code_huffman.py
+++ code_huffman.py
@@ -1,5 +1,5 @@
-
-
+import os
+import json
 from pprint import pprint
 
 
@@ -7,8 +7,18 @@ class CodeHuffman:
     """
     class which is dedicated to work with Huffman encoding/decoding
     """
-    def __init__(self, message:str) -> None:
+    def __init__(self, message:str, file_path:str='') -> None:
         self.message = message
+        self.file_path = file_path
+
+    @staticmethod
+    def check_previous_usage(path) -> None:
+        """
+        Static method which is dedicated to create folders if neccessary
+        Input:  path = path to the selected files
+        Output: folder on the an
+        """
+        os.path.exists(path) or os.mkdir(path)
 
     def develop_dict_count(self) -> dict:
         """
@@ -127,10 +137,91 @@ class CodeHuffman:
         for k, v in sorted(([k, v] for k, v in value_dict.items()), key=lambda x: len(x[1]), reverse=True):
             value_return = value_return.replace(f'{v} ', k)
         return value_return
+    
+    @staticmethod
+    def produce_binary_normalizing(value_dict:dict) -> set:
+        """
+        Static method which is dedicated to develop the binary with the normilized size of it
+        Input:  value_dict = dictionary with the selected calculations
+        Output: set with selected values of the dictionary and its own size of the 8
+        """
+        def develop_range(len_max:int) -> set:
+            for i in range(1, 10000000000000000000000):
+                if 8*i > len_max:
+                    return 8*i
+        len_max = max(len(f) for f in value_dict.values())
+        bit_size = develop_range(len_max)
+        value_dict = {
+            k: (bit_size - len(v))*"1" + v 
+            for k, v in value_dict.items() 
+        }
+        ret = {
+            k: bin(int(v, 2))
+            for k, v in value_dict.items()
+        }
+        ret.update({
+            "size": bit_size + 2
+        })
+        return ret
 
+    def produce_encoding_file(self) -> None:
+        """
+        Method which is dedicated to produce the basic encoding files to the binary file of it
+        Input:  None
+        Output: we developed the text file to the simple binary file
+        """
+        # self.check_previous_usage(os.path.join(os.getcwd(), 'storage'))
+        if not os.path.exists(self.file_path):
+            return
+        with open(self.file_path, 'r') as file_read:
+            self.message = file_read.read()
+        value_dict = self.develop_dict_count()
+        value_list = self.develop_letter_popular(value_dict)
+        value_dict = self.produce_tree_basic(value_list)
+        value_dict = self.produce_tree_reconstruction(value_dict)
+        value_dict = self.produce_binary_normalizing(value_dict)
+        value_bytes = ''.join(
+            value_dict.get(l, '0')
+            for l in self.message
+        )
+        value_bytes = value_bytes.encode()
+        with open(f"{self.file_path}.bin", 'wb') as file_write:
+            file_write.write(value_bytes)
+        with open(f"{self.file_path}.json", 'w') as json_write:
+            json.dump(
+                value_dict, 
+                json_write, 
+                indent=4
+            ) 
+
+    def produce_decoding_file(self) -> None:
+        """
+        Method which is dedicated to produce the basic decoding of the file
+        Input:  None
+        Output: we developed the decoded file from it
+        """
+        value_path = f"{self.file_path}.bin"
+        value_path_json = f"{self.file_path}.json"
+        if not os.path.exists(value_path) or not os.path.exists(value_path_json):
+            print('Files not present')
+            return
+        with open(value_path, 'rb') as file_read, open(value_path_json, 'r')  as json_read:
+            used = file_read.read().decode()
+            value_dict = json.load(json_read)
+            value_dict = {v:k for k, v in value_dict.items()}
+        used = ''.join(value_dict.get(f"0b{f}") for f in used.split('0b')[1:])
+        with open(os.path.join(os.getcwd(), 'storage', 'new.txt'), 'w') as file_new:
+            file_new.write(used)
+        #TODO complete the mistake
+        # for f, k in zip(used, self.message):
+        #     print(f, k, f==k)
+        #     print('-----------------------')
+        
 if __name__ == '__main__':
     tst = input()
-    a = CodeHuffman(tst)
+    file = os.path.join(os.getcwd(), 'storage', 'tst.txt')
+    a = CodeHuffman(tst, file)
     encoded, encoded_dict = a.produce_encoding_basic()
     tst_check = a.produce_decoding_basic(encoded, encoded_dict)
-    
+    a.produce_encoding_file()
+    a.produce_decoding_file()
